eb0a045ad4b3336201431821c877e7e620ae8c37
Nitesh <85004498+Niteshmeena9672@users.noreply.github.com>
2022-12-08 21:01:44 +0530

    Delete File Organizer directory

--- File Organizer/file_format.js
+++
@@ -1,6 +0,0 @@
-let types = {
-    media: ["mp4", "mkv"],
-    archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
-    documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
-    app: ['exe', 'dmg', 'pkg', "deb"]
-}
